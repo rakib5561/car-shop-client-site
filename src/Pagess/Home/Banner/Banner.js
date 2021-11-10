f53c53ddd307d0cbbb90d6fdf338a99ecd87217d
Md <mdrakib5561@gmail.com>
2021-11-10 18:04:57 +0600

    banner added

--- src/Pagess/Home/Banner/Banner.js
+++ src/Pagess/Home/Banner/Banner.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import './Banner.css';
+import banner from '../../../img/slide-01.jpg'
 
 const Banner = () => {
     return (
-        <div>
-            <h1>Banner</h1>
+        <div className="banner-part">
+
+            <div className="banner-container">
+                    
+                    <h1>Accelerating <br /> 
+                    the <span>Future</span>
+                    </h1>
+                    <p>We offer over 9,500 new and used vehicles and if you <br />
+                    need help finding nything we have a 24/7 team
+                    </p>
+                    <button className="btn-style">Explore</button>
+
+            </div>
+            
         </div>
     );
 };
